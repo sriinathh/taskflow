491692965d19d0ec791006d312ca0a8f7f5c0c4e
sriinathh <potharajusrinathh@gmail.com>
2026-02-07 13:02:24 +0530

    Update client API to use production backend URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://taskmanager-server-mqyj.onrender.com/api',
 });
 
 // Add interceptor to include token
